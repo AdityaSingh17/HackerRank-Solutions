6838a799056b6603ebce0a70ead5b19069e99f97
AdityaSingh17 <adityarsingh17@gmail.com>
2020-09-02 21:03:59 +0530

    Added solution to Python Introduction problems - II

---
+++ Python/01. Introduction/04. Python: Division/Solution.py
@@ -0,0 +1,8 @@
+# Python: Division
+# Problem Link: https://www.hackerrank.com/challenges/python-division/problem
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    print(a // b)  # integer division
+    print(a / float(b))  # float division
