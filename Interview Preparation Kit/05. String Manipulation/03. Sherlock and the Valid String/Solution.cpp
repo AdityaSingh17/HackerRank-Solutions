3174a297f72bdf7b4bebfbc891a5c62312401ff0
AdityaSingh17 <adityarsingh17@gmail.com>
2020-10-25 11:36:07 +0530

    Added solution to String Manipulation Challenges - II

---
+++ Interview Preparation Kit/05. String Manipulation/03. Sherlock and the Valid String/Solution.cpp
@@ -0,0 +1,67 @@
+// Sherlock and the Valid String
+// Problem Link: https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the isValid function below.
+string isValid(string s)
+{
+    const char *s_chars;
+    int max_occur;
+    bool removed_char;
+    vector<int> occur(26);
+
+    s_chars = s.c_str();
+    max_occur = -1;
+    removed_char = false;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        occur[s_chars[i] - 'a']++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (occur[i] == 0)
+        {
+            continue;
+        }
+        else if (max_occur == -1)
+        {
+            max_occur = occur[i];
+            continue;
+        }
+        else if (occur[i] == max_occur)
+        {
+            continue;
+        }
+        else if (!removed_char)
+        {
+            removed_char = !removed_char;
+            continue;
+        }
+        else
+        {
+            return "NO";
+        }
+    }
+
+    return "YES";
+}
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = isValid(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
